01ffa248ad924baa529b19c88fa8cfc717e4f154
Johannes Siedersberger <johannes.siedersberger@gmail.com>
2020-08-21 08:28:03 +0200

    Added Media Card

--- techquiz/src/components/QuestionComponent.js
+++ techquiz/src/components/QuestionComponent.js
@@ -1,52 +1,77 @@
-import React, { Component } from 'react';
-import { CardMedia } from '@material-ui/core';
+import React, {Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const useStyles = makeStyles((theme) => ({
+  const useStyles = makeStyles({
     root: {
       maxWidth: 345,
     },
     media: {
-      height: 0,
-      paddingTop: '56.25%', // 16:9
+      height: 140,
     },
-    expand: {
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-      }),
-    },
-    expandOpen: {
-      transform: 'rotate(180deg)',
-    },
-    avatar: {
-      backgroundColor: red[500],
-    },
-  }));
-
-  function ImageCard(){
+  });
+  
+  function MediaCard() {
     const classes = useStyles();
-    return(
-        <div>
-            <CardMedia 
+  
+    return (
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
             className={classes.media}
-            title="Tech Questions"
-            image="https://easyhonestfinancial.com/wp-content/uploads/2019/01/TECH.jpg"
-            />
-        </div>
+            image="https://www.riddle.com/imageservice/q_80,f_auto,c_fill,w_960,h_540/yucp0bhylyfxnkacespt"
+            title="TEchQuestion Title"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Cars
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+                Which was the first company in California
+                to be granted a permit to allow driverless vehicles on public roads?
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button variant="contained" size="small" color="primary">
+            Cruise
+          </Button>
+          <Button variant="contained" size="small" color="primary">
+            Tesla
+          </Button>
+          <Button variant="contained" size="small" color="primary">
+            Zoox
+          </Button>
+          <Button variant="contained" size="small" color="primary">
+            Waymo
+          </Button>
+        </CardActions>
+      </Card>
     );
   }
+
 class QuestionsComponent extends Component{
     constructor(props){
         super(props);
-        
     }
     
     render(){
         return(
-            <ImageCard/>
+            <div className="container">
+               <div className="">
+                    <div className="col-sm">
+                        <MediaCard/>
+                    </div>
+               </div>
+            </div>
+            
         )
     }
 }
