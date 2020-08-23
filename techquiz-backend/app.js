7ece212592b26800014027d777679ccf44d5631c
Johannes Siedersberger <johannes.siedersberger@gmail.com>
2020-08-23 15:11:48 +0200

    Moved Route to seperate file, Created folder structure

--- techquiz-backend/app.js
+++ techquiz-backend/app.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const PORT = 4000;
-const questionRoutes = express.Router();
-
-let Question = require('./questionmodel');
+const questionRouter = require('./routes/questionRouter');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,60 +16,7 @@ connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 });
 
-questionRoutes.route('/').get((req, res) => {
-    Question.find((err, questions) => {
-        if(err){
-            console.log(err);
-        } else{
-            res.json(questions);
-        }
-    });
-});
-
-questionRoutes.route('/:id').get((req, res) => {
-    Question.find((err, questions) => {
-        if(err){
-            console.log(err);
-        } else{
-            res.json(questions);
-        }
-    })
-});
-
-questionRoutes.route('/update/:id').post((req, res) => {
-    Question.findById(req.params.id, (err, question) => {
-        if(!question){
-            res.status(404).send("data not found");
-        }
-        else{
-            question.category = req.body.category;
-            question.question = req.body.question;
-            question.image = req.body.image;
-            question.answers = req.body.answers;
-            question.correctAnswer = req.body.correctAnswer;
-
-            question.save().then(question => {
-                res.json('Question Updated');
-            }).catch(err => {
-                res.status(400).send('Update not possible');
-            });
-
-        }
-    })
-});
-
-questionRoutes.route('/add').post((req, res) => {
-    let question = new Question(req.body);
-    question.save()
-        .then(question => {
-            res.status(200).json({'question' : 'qustion added successfully'});
-        })
-        .catch(err => {
-            res.status(400).send('adding new question failed');
-        });
-});
-
-app.use('/questions', questionRoutes);
+app.use('/questions', questionRouter);
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
